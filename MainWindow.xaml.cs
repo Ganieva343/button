4ab93a9c195dcaffadd417a8a42305faad6b280e
root <root@USER3>
2022-10-26 10:00:47 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace button
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Кнопка нажата");
+        }
+
+        private void acceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Действие выполнено");
+        }
+
+        private void escButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close(); // закрытие окна
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(checkBox.Content.ToString() + " отмечен");
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(checkBox.Content.ToString() + " не отмечен");
+        }
+
+        private void checkBox_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(checkBox.Content.ToString() + " в неопределенном состоянии");
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton pressed = (RadioButton)sender;
+            MessageBox.Show(pressed.Content.ToString());
+        }
+
+        
+    }
+}
